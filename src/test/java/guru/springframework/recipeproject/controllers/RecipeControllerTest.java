9153a81182526d3ba71eb5d80b649dc556bc95f9
yesiamanerd <hepsubah>
2020-05-28 09:57:36 -0400

    added 400 exception error page and message - Number format exception

--- src/test/java/guru/springframework/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipeproject/controllers/RecipeControllerTest.java
@@ -67,6 +67,16 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormatError() throws Exception {
+
+        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
